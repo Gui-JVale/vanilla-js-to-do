ca8d0f8c88427ae86c25b797349aa459614f61c9
Gui J Vale <guijvale@Guis-MacBook-Pro.local>
2020-07-09 12:14:36 -0700

    refactored ListView render and init methods to be more self discriptive and modular

--- src/views/list.view.js
+++ src/views/list.view.js
@@ -3,7 +3,44 @@ import { default as Controller } from '../controllers/controller';
 
 class ListView extends View {
   init() {
-    // Set variables / DOM elements
+    this.setupProps(); // Set variables / DOM elements
+    this.setupEventListeners(); // Attach event listeners
+    Controller.subscribe(this.render, 'modelUpdate', this); // Subscribe to model
+
+    // Render
+    return this.render(Controller.getList());
+  }
+
+  /*
+   *
+   * @param model to do list
+   * Builds "li" for each task and append full list
+   * to DOM
+   */
+  render(model) {
+    const frag = document.createDocumentFragment();
+    let i = model.length - 1;
+    let itemNode;
+    let item;
+    let childs;
+
+    this.listNode.innerHTML = '';
+    this.input.value = '';
+
+    for (i; i >= 0; i -= 1) {
+      item = model[i];
+
+      // Build "LI" element w/ its children
+      childs = ListView.buildChildNodes(item, i);
+      itemNode = ListView.buildItemNode(childs, i);
+      this.addDragAndDrop(itemNode); // Add drag and drop func.
+
+      frag.appendChild(itemNode); // Append to frag
+    }
+    return this.listNode.appendChild(frag); // Append to DOM
+  }
+
+  setupProps() {
     this.listNode = document.getElementById('list-view');
     this.input = document.getElementById('task');
     this.submit = document.getElementById('submit-form');
@@ -14,114 +51,14 @@ class ListView extends View {
     this.draggedOver = null;
     this.shiftX = null;
     this.shiftY = null;
-
     this.dropzone.classList.add('list__drag-dropzone');
+  }
 
-    // Attach event listeners
+  setupEventListeners() {
     this.submit.addEventListener('click', this.handleSubmit.bind(this));
     this.clear.addEventListener('click', function () {
       Controller.updateList('empty');
     });
-
-    // Subscribe to events
-    Controller.subscribe(this.render, 'modelUpdate', this);
-
-    // Render
-    this.render(Controller.getList());
-  }
-
-  /*
-   *
-   * Receives the list as param.
-   * Creates DOM elements for each task
-   * Add Event listeners to elements
-   * Returns by appending the fragment to the DOM
-   */
-  render(model) {
-    const frag = document.createDocumentFragment();
-    let i = model.length - 1;
-    let itemNode;
-    let textWrapper;
-    let dragIcon;
-    let checkbox;
-    let label;
-    let item;
-    let deleteIcon;
-
-    this.listNode.innerHTML = '';
-    this.input.value = '';
-
-    for (i; i >= 0; i -= 1) {
-      item = model[i];
-
-      // Create text wrapper for task
-      textWrapper = View.createNodeWithText('span', item.task);
-      textWrapper.draggable = false;
-      textWrapper.classList.add('list__task');
-      textWrapper.addEventListener('dragstart', function (e) {
-        e.preventDefault();
-        e.cancelBubble();
-      });
-
-      // Create drag icon
-      dragIcon = document.createElement('ion-icon');
-      dragIcon.setAttribute('name', 'apps-outline');
-      dragIcon.classList.add('list__drag-icon');
-      dragIcon.addEventListener('mousedown', function () {
-        this.parentElement.setAttribute('draggable', true);
-      });
-
-      // Create checkbox input
-      checkbox = document.createElement('input');
-      checkbox.style.display = 'none';
-      checkbox.classList.add('list__checkbox');
-      checkbox.setAttribute('type', 'checkbox');
-      checkbox.setAttribute('id', `toggle-task-${i}`);
-      if (item.completed) checkbox.checked = true;
-
-      // Create checkbox label
-      label = document.createElement('label');
-      label.classList.add('list__label');
-      label.setAttribute('for', `toggle-task-${i}`);
-      label.addEventListener('dragstart', ListView.stop);
-      label.addEventListener(
-        'click',
-        (function (index) {
-          return function () {
-            Controller.updateList('toggleComplete', index);
-          };
-        })(i)
-      );
-
-      // Create delete icon
-      deleteIcon = document.createElement('ion-icon');
-      deleteIcon.setAttribute('name', 'trash-outline');
-      deleteIcon.classList.add('list__delete-icon');
-      deleteIcon.addEventListener('dragstart', ListView.stop);
-      deleteIcon.addEventListener(
-        'click',
-        (function (index, _this) {
-          return function () {
-            Controller.updateList('remove', index);
-            return _this.render();
-          };
-        })(i, this)
-      );
-
-      // Create item node
-      itemNode = document.createElement('li');
-      itemNode.dataset.index = i;
-      itemNode.appendChild(dragIcon);
-      itemNode.appendChild(checkbox);
-      itemNode.appendChild(textWrapper);
-      itemNode.appendChild(label);
-      itemNode.appendChild(deleteIcon);
-      itemNode.classList.add('list__item');
-      this.dragAndDrop(dragIcon);
-
-      frag.appendChild(itemNode); // Append to frag
-    }
-    return this.listNode.appendChild(frag); // Append to DOM
   }
 
   handleSubmit(e) {
@@ -130,6 +67,96 @@ class ListView extends View {
 
     if (!value) return this;
     return Controller.updateList('add', { completed: false, task: value });
+  }
+
+  addDragAndDrop(itemNode) {
+    const dragIcon = itemNode.querySelector('.list__drag-icon');
+    return this.dragAndDrop(dragIcon);
+  }
+
+  static buildItemNode(childs, index) {
+    const itemNode = document.createElement('li');
+    itemNode.dataset.index = index;
+    childs.forEach((child) => itemNode.appendChild(child));
+    itemNode.classList.add('list__item');
+
+    return itemNode;
+  }
+
+  static buildChildNodes(item, i) {
+    const childs = [];
+
+    childs.push(ListView.buildDragIcon());
+    childs.push(ListView.buildTextWrapper(item.task));
+    childs.push(ListView.buildCheckbox(item.completed, i));
+    childs.push(ListView.buildLabel(i));
+    childs.push(ListView.buildDeleteIcon(i));
+
+    return childs;
+  }
+
+  static buildTextWrapper(text) {
+    const textWrapper = View.createNodeWithText('span', text);
+    textWrapper.draggable = false;
+    textWrapper.classList.add('list__task');
+    textWrapper.addEventListener('dragstart', function (e) {
+      e.preventDefault();
+      e.cancelBubble();
+    });
+
+    return textWrapper;
+  }
+
+  static buildDragIcon() {
+    const dragIcon = document.createElement('ion-icon');
+    dragIcon.setAttribute('name', 'apps-outline');
+    dragIcon.classList.add('list__drag-icon');
+    dragIcon.addEventListener('mousedown', function () {
+      this.parentElement.setAttribute('draggable', true);
+    });
+    return dragIcon;
+  }
+
+  static buildCheckbox(checked, index) {
+    const checkbox = document.createElement('input');
+    checkbox.style.display = 'none';
+    checkbox.classList.add('list__checkbox');
+    checkbox.setAttribute('type', 'checkbox');
+    checkbox.setAttribute('id', `toggle-task-${index}`);
+    if (checked) checkbox.checked = true;
+
+    return checkbox;
+  }
+
+  static buildLabel(i) {
+    const label = document.createElement('label');
+    label.classList.add('list__label');
+    label.setAttribute('for', `toggle-task-${i}`);
+    label.addEventListener('dragstart', ListView.stop);
+    label.addEventListener(
+      'click',
+      (function (index) {
+        return function () {
+          Controller.updateList('toggleComplete', index);
+        };
+      })(i)
+    );
+
+    return label;
+  }
+
+  static buildDeleteIcon(index) {
+    const deleteIcon = document.createElement('span');
+    const trash = document.createElement('ion-icon');
+    trash.setAttribute('name', 'trash-outline');
+    deleteIcon.classList.add('list__delete-icon');
+    deleteIcon.appendChild(trash);
+    deleteIcon.addEventListener('dragstart', ListView.stop);
+    deleteIcon.addEventListener('click', function () {
+      Controller.updateList('remove', index);
+    });
+
+    return deleteIcon;
   }
 
   /*
